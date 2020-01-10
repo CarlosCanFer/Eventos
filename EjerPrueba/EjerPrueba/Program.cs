66cfabd3b8348136ba3f439a9d4fceaa87963452
CarlosCanFer <54689616+CarlosCanFer@users.noreply.github.com>
2020-01-10 09:38:08 -0600

    Se sube la versión 1.1

--- EjerPrueba/EjerPrueba/Program.cs
+++ EjerPrueba/EjerPrueba/Program.cs
@@ -1,74 +1,98 @@
 ﻿using System;
 using System.IO;
+using EjerPrueba.Interfaces;
 
 namespace EjerPrueba
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main principal
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
         {
-            string _cubicacionTexto= @"C:\Datos\datos.txt";
+            ValidarDatos();
+            Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Método que se encarga de leer el archivo de datos,
+        /// </summary>
+        /// <returns>Devuelve un conjunto de datos</returns>
+        private static String[] DatosRecuperados() {
+            string _cubicacionTexto = PedirRutaArchivo();
             string _ctextoentrada = File.ReadAllText(_cubicacionTexto);
 
-            string[] _cdatostotal = _ctextoentrada.Split(new string[] {Environment.NewLine},StringSplitOptions.None);
+            string[] _cdatostotal = _ctextoentrada.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+            return _cdatostotal;
+        }
 
-            foreach (string _cvalor in _cdatostotal)
+        /// <summary>
+        /// Método encargado de obtener la ruta del archivo a evaluar
+        /// </summary>
+        /// <returns>Regresa una cadena con la ruta del archivo</returns>
+        public static String PedirRutaArchivo()
+        {
+            string _crutaArchivo;
+
+            Console.WriteLine("Arraste la ruta del archivo que desea comprobar:");
+            _crutaArchivo =Console.ReadLine();
+
+            return _crutaArchivo;
+
+        }       
+
+        /// <summary>
+        /// Método encargado de realizar la validacíon.
+        /// </summary>
+        public static void ValidarDatos() {
+            String[] _dDatos = DatosRecuperados();
+            IValidador<String> _objconsultas = new Validador();
+
+            try
             {
-                string _cnombreEvento;
-                DateTime _cfechaEvento;
-                DateTime _dHoy = DateTime.Now;
-                int _ianios, _imeses, _idias, _ihoras;               
-
-                string[] _cENombrefecha = _cvalor.Split(',');
-                _cnombreEvento = _cENombrefecha[0];
-                _cfechaEvento = DateTime.Parse(_cENombrefecha[1]);
-
-                if (_dHoy > _cfechaEvento)
+                foreach (string _cvalor in _dDatos)
                 {
-                    if (_dHoy.Year > _cfechaEvento.Year)
-                    {
-                        _ianios = (_dHoy.Year) - (_cfechaEvento.Year);
+                    string _cnombreEvento;
+                    DateTime _cfechaEvento;
+                    DateTime _dHoy = DateTime.Now;                    
+                    string[] _cENombrefecha = _cvalor.Split(',');
+                    _cnombreEvento = _cENombrefecha[0];
+                    _cfechaEvento = DateTime.Parse(_cENombrefecha[1]);
 
-                        Console.WriteLine("El evento " + _cnombreEvento + " Ocurrio hace: " + _ianios + " años");
+                    if (_dHoy.Month != _cfechaEvento.Month)
+                    {
+                        _objconsultas.ValidaMes(_cvalor);
                     }
-                    else
-                    {
-                        if (_dHoy.Month > _cfechaEvento.Month)
+                    else {
+                        if (_dHoy.Day != _cfechaEvento.Day)
                         {
-                            _imeses = (_dHoy.Month) - (_cfechaEvento.Month);
-                            Console.WriteLine("El evento " + _cnombreEvento + " Ocurrio hace: " + _imeses + " meses");
-
+                            _objconsultas.ValidaDia(_cvalor);
                         }
                         else {
-                            _idias = (_dHoy.Day) - (_cfechaEvento.Day);
-                            Console.WriteLine("El evento " + _cnombreEvento + " Ocurrio hace: " + _idias + " días");
+                            if (_dHoy.Hour != _cfechaEvento.Hour)
+                            {
+                                _objconsultas.ValidaHora(_cvalor);
+                            }
+                            else {
+                                if (_dHoy.Minute != _cfechaEvento.Minute)
+                                {
+                                    _objconsultas.ValidaMinuto(_cvalor);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("El evento " + _cENombrefecha[0] + " Ocurrio hace un momento");
+                                }
+                            }
                         }
-                    }
+                    }                                     
                 }
-                else {
-
-                    if (_dHoy.Year < _cfechaEvento.Year)
-                    {
-                        _ianios = (_cfechaEvento.Year) - (_dHoy.Year);
-                        Console.WriteLine("El evento " + _cnombreEvento + " Ocurrirá dentro de: " + _ianios + " años");
-                    }
-                    else
-                    {
-                        if (_dHoy.Month < _cfechaEvento.Month)
-                        {
-                            _imeses = (_cfechaEvento.Month)-(_dHoy.Month);
-                            Console.WriteLine("El evento " + _cnombreEvento + " Ocurrirá dentro de: " + _imeses + " meses");
-
-                        }
-                        else
-                        {
-                            _idias = (_cfechaEvento.Day)- (_dHoy.Day);
-                            Console.WriteLine("El evento " + _cnombreEvento + " Ocurrirá dentro de: " + _idias + " días");
-                        }
-                    }                  
-                }
-            }          
-            Console.ReadLine();
+            }
+            catch
+            {
+                Console.WriteLine("Los datos ingresados en el archivo son incorrectos");
+            }
         }
     }
 }
